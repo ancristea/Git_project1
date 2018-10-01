5c2cd90c032dec6dedb25ca38beda340b368e8ad
Andrei.Cristea <andrei.cristea@endava.com>
2018-10-01 17:42:25 +0300

    5th commit

--- src/main/java/FirstC.java
+++ src/main/java/FirstC.java
@@ -2,6 +2,7 @@ public class FirstC {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("jfkljdfngidjhxdfgkln");
+        System.out.println("commit for conflict");
 
     }
 }
